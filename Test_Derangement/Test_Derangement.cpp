4fba8279ede01303d88a20188889ef71b540f1b3
Krol Anastasia <90043995+AnastasiaLork@users.noreply.github.com>
2023-04-13 22:57:19 +0300

    Добавление ссылок на главный проект в тестовые проекты

--- Test_Derangement/Test_Derangement.cpp
+++ Test_Derangement/Test_Derangement.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../permutationsWithFixedPoints/permutationsWithFixedPoints.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
