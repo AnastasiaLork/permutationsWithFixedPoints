df50203a68fe64c3125261c74dbe1da062879934
Krol Anastasia <90043995+AnastasiaLork@users.noreply.github.com>
2023-04-13 22:43:29 +0300

    Создание пустых проектов для тестов

---
+++ Test_Factorial/Test_Factorial.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestFactorial
+{
+	TEST_CLASS(TestFactorial)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
