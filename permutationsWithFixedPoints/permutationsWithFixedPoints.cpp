d3f451a447d2dac4539dfc43c52f851ef40abed3
Krol Anastasia <90043995+AnastasiaLork@users.noreply.github.com>
2023-04-14 10:22:17 +0300

    Добавление заглушек в реализации функций

---
+++ permutationsWithFixedPoints/permutationsWithFixedPoints.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <locale.h>
+
+int factorial(int n)
+{
+	return 0;
+}
+
+int permutationsWithFixedPoints(int n, int k)
+{
+	return 0;
+}
+
+int combinationWithoutRepeat(int n, int k)
+{
+	return 0;
+}
+
+int derangement(int n)
+{
+	return 0;
+}
