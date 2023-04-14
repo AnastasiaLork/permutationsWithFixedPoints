1ad979cab3dd075060ad87936c488d08ef448cbd
Krol Anastasia <90043995+AnastasiaLork@users.noreply.github.com>
2023-04-14 13:46:30 +0300

    Реализация набора тестов для функции Combination

--- Test_CombinationWithoutRepeat/Test_CombinationWithoutRepeat.cpp
+++ Test_CombinationWithoutRepeat/Test_CombinationWithoutRepeat.cpp
@@ -20,5 +20,84 @@ namespace TestCombinationWithoutRepeat
 			Assert::AreEqual(expNumberCombination, resultNumberCombination);
 		}
 
+		TEST_METHOD(MinSet_OneSelectedElement)
+		{
+			int setOfElements = 1;
+			int numberSelectedElements = 1;
+			int expNumberCombination = 1;
+
+			int resultNumberCombination = combinationWithoutRepeat(setOfElements, numberSelectedElements);
+			Assert::AreEqual(expNumberCombination, resultNumberCombination);
+		}
+
+		TEST_METHOD(MinSet_ZeroSelectedElements)
+		{
+			int setOfElements = 1;
+			int numberSelectedElements = 0;
+			int expNumberCombination = 1;
+
+			int resultNumberCombination = combinationWithoutRepeat(setOfElements, numberSelectedElements);
+			Assert::AreEqual(expNumberCombination, resultNumberCombination);
+		}
+
+		TEST_METHOD(MaxSet)
+		{
+			int setOfElements = 9;
+			int numberSelectedElements = 3;
+			int expNumberCombination = 84;
+
+			int resultNumberCombination = combinationWithoutRepeat(setOfElements, numberSelectedElements);
+			Assert::AreEqual(expNumberCombination, resultNumberCombination);
+		}
+
+		TEST_METHOD(MaxNumberSelectedElements)
+		{
+			int setOfElements = 6;
+			int numberSelectedElements = 6;
+			int expNumberCombination = 1;
+
+			int resultNumberCombination = combinationWithoutRepeat(setOfElements, numberSelectedElements);
+			Assert::AreEqual(expNumberCombination, resultNumberCombination);
+		}
+
+		TEST_METHOD(NoSelectedElements)
+		{
+			int setOfElements = 6;
+			int numberSelectedElements = 0;
+			int expNumberCombination = 1;
+
+			int resultNumberCombination = combinationWithoutRepeat(setOfElements, numberSelectedElements);
+			Assert::AreEqual(expNumberCombination, resultNumberCombination);
+		}
+
+		TEST_METHOD(NegativeSet)
+		{
+			int setOfElements = -6;
+			int numberSelectedElements = 3;
+			int expNumberCombination = -1;
+
+			int resultNumberCombination = combinationWithoutRepeat(setOfElements, numberSelectedElements);
+			Assert::AreEqual(expNumberCombination, resultNumberCombination);
+		}
+
+		TEST_METHOD(NegativeNumberSelectedElement)
+		{
+			int setOfElements = 6;
+			int numberSelectedElements = -3;
+			int expNumberCombination = -1;
+
+			int resultNumberCombination = combinationWithoutRepeat(setOfElements, numberSelectedElements);
+			Assert::AreEqual(expNumberCombination, resultNumberCombination);
+		}
+
+		TEST_METHOD(NumberSelectedElementMoreThanSet)
+		{
+			int setOfElements = 6;
+			int numberSelectedElements = 9;
+			int expNumberCombination = -1;
+
+			int resultNumberCombination = combinationWithoutRepeat(setOfElements, numberSelectedElements);
+			Assert::AreEqual(expNumberCombination, resultNumberCombination);
+		}
 	};
 }
