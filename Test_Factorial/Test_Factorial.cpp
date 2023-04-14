2243f94dd614a07346f069405f1260e0f857ba27
Krol Anastasia <90043995+AnastasiaLork@users.noreply.github.com>
2023-04-14 10:24:20 +0300

    Реализация тестов для функции factorial

--- Test_Factorial/Test_Factorial.cpp
+++ Test_Factorial/Test_Factorial.cpp
@@ -8,9 +8,50 @@ namespace TestFactorial
 	TEST_CLASS(TestFactorial)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		TEST_METHOD(SampleTest)
 		{
+			int number = 4;
+			int expFactorial = 24;
+
+			int resultFactorial = factorial(number);
+			Assert::AreEqual(expFactorial, resultFactorial);
+		}
+
+		TEST_METHOD(MinNumber)
+		{
+			int number = 0;
+			int expFactorial = 1;
+
+			int resultFactorial = factorial(number);
+			Assert::AreEqual(expFactorial, resultFactorial);
+		}
+
+		TEST_METHOD(MaxNumber)
+		{
+			int number = 9;
+			int expFactorial = 362880;
+
+			int resultFactorial = factorial(number);
+			Assert::AreEqual(expFactorial, resultFactorial);
+		}
+
+		TEST_METHOD(NumberIsOne)
+		{
+			int number = 1;
+			int expFactorial = 1;
+
+			int resultFactorial = factorial(number);
+			Assert::AreEqual(expFactorial, resultFactorial);
+		}
+
+		TEST_METHOD(NegativeNumber)
+		{
+			int number = -4;
+			int expFactorial = -1;
+
+			int resultFactorial = factorial(number);
+			Assert::AreEqual(expFactorial, resultFactorial);
 		}
 	};
 }
