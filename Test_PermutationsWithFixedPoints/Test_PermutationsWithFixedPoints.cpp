d6518267e7c3b8b851294f308302495323209389
Krol Anastasia <90043995+AnastasiaLork@users.noreply.github.com>
2023-04-14 14:33:13 +0300

    Реализация тестов главной функции permutationsWithFixedPoints

--- Test_PermutationsWithFixedPoints/Test_PermutationsWithFixedPoints.cpp
+++ Test_PermutationsWithFixedPoints/Test_PermutationsWithFixedPoints.cpp
@@ -8,9 +8,96 @@ namespace TestPermutationsWithFixedPoints
 	TEST_CLASS(TestPermutationsWithFixedPoints)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		TEST_METHOD(SampleTest)
 		{
+			int sizePermutation = 6;
+			int numberOfPoints = 3;
+			int expNumberPermutation = 40;
+
+			int resultNumberPermutation = permutationsWithFixedPoints(sizePermutation, numberOfPoints);
+			Assert::AreEqual(expNumberPermutation, resultNumberPermutation);
+		}
+
+		TEST_METHOD(MinSize_onePoint)
+		{
+			int sizePermutation = 1;
+			int numberOfPoints = 1;
+			int expNumberPermutation = 1;
+
+			int resultNumberPermutation = permutationsWithFixedPoints(sizePermutation, numberOfPoints);
+			Assert::AreEqual(expNumberPermutation, resultNumberPermutation);
+		}
+
+		TEST_METHOD(MinSize_noPoints)
+		{
+			int sizePermutation = 1;
+			int numberOfPoints = 0;
+			int expNumberPermutation = 0;
+
+			int resultNumberPermutation = permutationsWithFixedPoints(sizePermutation, numberOfPoints);
+			Assert::AreEqual(expNumberPermutation, resultNumberPermutation);
+		}
+
+		TEST_METHOD(MaxSize)
+		{
+			int sizePermutation = 9;
+			int numberOfPoints = 3;
+			int expNumberPermutation = 22260;
+
+			int resultNumberPermutation = permutationsWithFixedPoints(sizePermutation, numberOfPoints);
+			Assert::AreEqual(expNumberPermutation, resultNumberPermutation);
+		}
+
+		TEST_METHOD(MaxNumberPoints)
+		{
+			int sizePermutation = 6;
+			int numberOfPoints = 6;
+			int expNumberPermutation = 1;
+
+			int resultNumberPermutation = permutationsWithFixedPoints(sizePermutation, numberOfPoints);
+			Assert::AreEqual(expNumberPermutation, resultNumberPermutation);
+		}
+
+		TEST_METHOD(NoPoints)
+		{
+			int sizePermutation = 6;
+			int numberOfPoints = 0;
+			int expNumberPermutation = 265;
+
+			int resultNumberPermutation = permutationsWithFixedPoints(sizePermutation, numberOfPoints);
+			Assert::AreEqual(expNumberPermutation, resultNumberPermutation);
+		}
+
+		TEST_METHOD(NegativeSize)
+		{
+			int sizePermutation = 1;
+			int numberOfPoints = 1;
+			int expNumberPermutation = 1;
+
+			int resultNumberPermutation = permutationsWithFixedPoints(sizePermutation, numberOfPoints);
+			Assert::AreEqual(expNumberPermutation, resultNumberPermutation);
+		}
+
+
+		TEST_METHOD(NegativeNumberPoints)
+		{
+			int sizePermutation = 1;
+			int numberOfPoints = 1;
+			int expNumberPermutation = 1;
+
+			int resultNumberPermutation = permutationsWithFixedPoints(sizePermutation, numberOfPoints);
+			Assert::AreEqual(expNumberPermutation, resultNumberPermutation);
+		}
+
+		TEST_METHOD(NumberPointsMoreThanSize)
+		{
+			int sizePermutation = 1;
+			int numberOfPoints = 1;
+			int expNumberPermutation = 1;
+
+			int resultNumberPermutation = permutationsWithFixedPoints(sizePermutation, numberOfPoints);
+			Assert::AreEqual(expNumberPermutation, resultNumberPermutation);
 		}
 	};
 }
