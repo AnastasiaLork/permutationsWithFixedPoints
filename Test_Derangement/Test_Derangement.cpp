31e818a9db356b160e06a78a7c063554435d8dbf
Krol Anastasia <90043995+AnastasiaLork@users.noreply.github.com>
2023-04-14 13:54:49 +0300

    Реализация тестов для функции Derangement

--- Test_Derangement/Test_Derangement.cpp
+++ Test_Derangement/Test_Derangement.cpp
@@ -9,9 +9,50 @@ namespace TestDerangement
 	TEST_CLASS(TestDerangement)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		TEST_METHOD(SampleTest)
 		{
+			int sizePermutation = 3;
+			int expNumberDerangement = 2;
+
+			int resultNumberDerangement = derangement(sizePermutation);
+			Assert::AreEqual(expNumberDerangement, resultNumberDerangement);
+		}
+
+		TEST_METHOD(MinSizePermutation)
+		{
+			int sizePermutation = 1;
+			int expNumberDerangement = 0;
+
+			int resultNumberDerangement = derangement(sizePermutation);
+			Assert::AreEqual(expNumberDerangement, resultNumberDerangement);
+		}
+
+		TEST_METHOD(MaxSizePermutation)
+		{
+			int sizePermutation = 9;
+			int expNumberDerangement = 133496;
+
+			int resultNumberDerangement = derangement(sizePermutation);
+			Assert::AreEqual(expNumberDerangement, resultNumberDerangement);
+		}
+
+		TEST_METHOD(SizePermutationIsOne)
+		{
+			int sizePermutation = 1;
+			int expNumberDerangement = 0;
+
+			int resultNumberDerangement = derangement(sizePermutation);
+			Assert::AreEqual(expNumberDerangement, resultNumberDerangement);
+		}
+
+		TEST_METHOD(NegativeSizePermutation)
+		{
+			int sizePermutation = -3;
+			int expNumberDerangement = -1;
+
+			int resultNumberDerangement = derangement(sizePermutation);
+			Assert::AreEqual(expNumberDerangement, resultNumberDerangement);
 		}
 	};
 }
